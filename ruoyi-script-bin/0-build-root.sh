7f8705a9c452f71f84170a5f12db8a37b1b1eab0
weibocy <weibocy@hotmail.com>
2022-10-17 21:51:03 +0800

    更新编译脚本参数，使用maven强制更新

--- ruoyi-script-bin/0-build-root.sh
+++ ruoyi-script-bin/0-build-root.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo mkdir -p ~/.m2/repository/
-docker run -it --rm --name my-maven-project -v ~/.m2/repository:/root/.m2/repository -v $(dirname $(pwd)):/usr/src/mymaven -w /usr/src/mymaven maven:3.6-amazoncorretto-8 mvn clean install
+docker run -it --rm --name my-maven-project -v ~/.m2/repository:/root/.m2/repository -v $(dirname $(pwd)):/usr/src/mymaven -w /usr/src/mymaven maven:3.6-amazoncorretto-8 mvn clean install -e -U
