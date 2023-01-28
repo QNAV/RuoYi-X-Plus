7ec9d9f1f086066773d452d700fcdcbcd1dd6150
weibocy <weibocy@hotmail.com>
2023-01-28 15:56:00 +0800

    update 优化 getLoginId 增加必要参数空校验

--- ruoyi-library-biz/src/main/java/com/ruoyi/biz/domain/model/BizLoginUser.java
+++ ruoyi-library-biz/src/main/java/com/ruoyi/biz/domain/model/BizLoginUser.java
@@ -126,6 +126,12 @@ public class BizLoginUser implements Serializable {
      * 获取登录id
      */
     public String getLoginId() {
+        if (userType == null) {
+            throw new IllegalArgumentException("用户类型不能为空");
+        }
+        if (userId == null) {
+            throw new IllegalArgumentException("用户ID不能为空");
+        }
         return userType + BizLoginHelper.JOIN_CODE + userId;
     }
 
