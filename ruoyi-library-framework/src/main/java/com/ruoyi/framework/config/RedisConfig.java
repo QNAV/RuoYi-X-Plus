654fa0bb2c2c98b1d25cce4b1c03285b6161933b
weibocy <weibocy@hotmail.com>
2023-02-01 10:15:00 +0800

    update 优化 去除无用继承

--- ruoyi-library-framework/src/main/java/com/ruoyi/framework/config/RedisConfig.java
+++ ruoyi-library-framework/src/main/java/com/ruoyi/framework/config/RedisConfig.java
@@ -78,7 +78,6 @@ public class RedisConfig extends CachingConfigurerSupport {
      * 自定义缓存管理器 整合spring-cache
      */
     @Bean
-    @Override
     public CacheManager cacheManager() {
         return new SpringCacheManager();
     }
